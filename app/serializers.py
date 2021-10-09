5c0d3a3973043ec06caa8b79aa2124b79e555e5b
Animesh Risal <animeshrisal99@gmail.com>
2021-10-09 19:43:40 +0545

    Added file upload

---
+++ app/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from app.models import AnalyzedFile, ExcelFile
+
+class ExcelFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExcelFile
+        fields = ['id', 'file']
+
+class AnalyzedFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnalyzedFile
+        fields = ['id', 'post', 'neg', 'compound']
+
