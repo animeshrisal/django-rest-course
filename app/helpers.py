6cbe3b38d9ca248cc3f06f2c9b5c368562559ecf
Animesh Risal <animeshrisal99@gmail.com>
2021-10-10 19:15:12 +0545

    Change number

--- app/helpers.py
+++ app/helpers.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 DEFAULT_PAGE = 1
-DEFAULT_PAGE_SIZE = 1
+DEFAULT_PAGE_SIZE = 10
 
 class StandardResultsSetPagination(PageNumberPagination):
     page = DEFAULT_PAGE
