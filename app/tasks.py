bfb2371fde3726088baf6bb6a45e0e693c3a265d
Animesh Risal <animeshrisal99@gmail.com>
2021-10-10 10:01:40 +0545

    Save data to analyze table

--- app/tasks.py
+++ app/tasks.py
@@ -3,10 +3,14 @@ from celery import shared_task
 import numpy as np
 import pandas as pd
 from sentiment import settings
+from .models import AnalyzedFile
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+from django.db import transaction
 
 @shared_task
 def analyze(file):
-
     #Get file name
     file_name = os.path.basename(file['file'])
     
@@ -14,7 +18,25 @@ def analyze(file):
     file_path = os.path.join(settings.MEDIA_ROOT + '/excel/' + file_name)
     
     #Read the file in pandas
-    df = pd.read_csv(file_path)
-    print(df)
+    df = pd.read_csv(file_path)   
+
+    sid = SentimentIntensityAnalyzer()
+    
+
+    #Atomic transactions will either save all entries or none if an error occurs
+    with transaction.atomic():
+        for index, row in df.iterrows():
+            sentiment_scores = sid.polarity_scores(row['text'])
+            AnalyzedFile.objects.create(
+                text = row['text'],
+                pos = sentiment_scores['pos'],
+                neg = sentiment_scores['neg'],
+                neu = sentiment_scores['neu'],
+                compound = sentiment_scores['compound'],
+                file_id = file['id']
+            )
+
+
+    
     
  
